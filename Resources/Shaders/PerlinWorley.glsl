97a3e19ff34cd59bf633d76229797491f49cf1e2
Berk Bavaş <berkbavas@gmail.com>
2022-08-25 07:09:26 +0300

    Attributions to Federico Vaccaro is added.

--- Resources/Shaders/PerlinWorley.glsl
+++ Resources/Shaders/PerlinWorley.glsl
@@ -16,6 +16,28 @@ uniform float frequenceMul[6u] = float[](2.0, 8.0, 14.0, 20.0, 26.0, 32.0);
 //Code from https://github.com/NadirRoGue
 //Special thanks https://github.com/NadirRoGue
 
+//MIT License
+
+//Copyright (c) 2018 Federico Vaccaro
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 float hash(int n)
 {
     return fract(sin(float(n) + 1.951) * 43758.5453123);
