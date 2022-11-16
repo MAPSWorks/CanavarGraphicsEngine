1270d06745dff903c744104ee3454709442322ea
Berk Bavaş <berkbavas@gmail.com>
2022-11-16 19:32:22 +0300

    "PostBuild.bat" is updated.

--- Simulator/PostBuild.bat
+++ Simulator/PostBuild.bat
@@ -1,4 +1,4 @@
-rmdir /s /q "%1\Resources"
-xcopy /E /Y "Simulator\Resources" "%1\Resources\"
-xcopy /E /Y "Engine\Resources" "%1\Resources\"
-xcopy /E /Y "Engine\Dependencies\assimp\bin\assimp-vc142-mt.dll" "%1"
+rmdir /s /q "%~1\Resources"
+xcopy /E /Y "Simulator\Resources" "%~1\Resources\"
+xcopy /E /Y "Engine\Resources" "%~1\Resources\"
+xcopy /E /Y "Engine\Dependencies\assimp\bin\assimp-vc142-mt.dll" "%~1"
